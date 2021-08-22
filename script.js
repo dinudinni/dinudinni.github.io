f42daca4294d2e9465e40b09f21c01d9cb6ca2f6
Dini Rizqi A <dinirizqidin@gmail.com>
2021-08-22 14:11:57 +0800

    Upload file(s) about website and stuff
    I upload 8 files that needed to run my website

---
+++ script.js
@@ -0,0 +1,10 @@
+
+window.addEventListener("load",addlisteners);
+
+function addlisteners(){
+    document.getElementById("magic").addEventListener("click", myfirst);
+}
+
+function myfirst(){
+    document.getElementById("ta-ra").innerHTML= "If you can not do it on your 20s, you can do it on your 30s. If you can not do it on your 30s, you can do it on your 40s (-Judy). You can try 1% a day as long as you are better than who you are yesterday. Do not ever envy to others, you do not even know how hard they try."
+}
